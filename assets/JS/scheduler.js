baee278d4ac3720b20ff1368a0a4cb0ea605049d
Vincent Romine <vinjrom@live.com>
2019-05-28 21:38:14 -0500

    added append from Firebase function

--- assets/JS/scheduler.js
+++ assets/JS/scheduler.js
@@ -1,25 +1,76 @@
-// Capture button Click
-$("#submit").on("click", function(event) {
-    // prevent page from refreshing when form tries to submit itself
-    event.preventDefault();
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDnmaPP77QcP9yVKdiS6uSAJXJeGYqydFU",
+        authDomain: "project1-cbdfd.firebaseapp.com",
+        databaseURL: "https://project1-cbdfd.firebaseio.com",
+        projectId: "project1-cbdfd",
+        storageBucket: "project1-cbdfd.appspot.com",
+        messagingSenderId: "461351271704",
+        appId: "1:461351271704:web:84d64932d88d7bd4"
+    };
 
-    // Capture user inputs and store them into variables
-    var train = {
-        name: $("#name-input").val().trim(),
-        destination: $("#dest-input").val().trim(),
-        frequency: $("#freq-input").val().trim(),
-        arrival: $("#time-input").val().trim(),
-    }
+    firebase.initializeApp(config);
 
-    // Console log each user input
-    console.log(train);
-    localStorage.clear();
-    localStorage.setItem('train', JSON.stringify(train));
+    var dataRef = firebase.database();
 
-    // Output all of the new information into the relevant HTML sections
-    $('#name-display').text(train.name);
-    $('#dest-display').text(train.destination);
-    $('#freq-display').text(train.frequency);
-    $('#arrival-display').text(train.arrival);
+    // Initial Values
+    var name = "";
+    var destination = "";
+    var frequency = 0;
+    var arrival = 0;
 
-})
+
+    // Capture button Click
+    $("#submit").on("click", function(event) {
+        // prevent page from refreshing when form tries to submit itself
+        event.preventDefault();
+
+        // Capture user inputs and store them into variables
+        name = $("#name-input").val().trim(),
+            destination = $("#dest-input").val().trim(),
+            frequency = $("#freq-input").val().trim(),
+            arrival = $("#time-input").val().trim(),
+
+            // Code for the push
+            dataRef.ref().push({
+                name: name,
+                destination: destination,
+                frequency: frequency,
+                arrival: arrival,
+            });
+
+        // Output all of the new information into the relevant HTML sections
+        // $('#name-display').text(train.name);
+        // $('#dest-display').text(train.destination);
+        // $('#freq-display').text(train.frequency);
+        // $('#arrival-display').text(train.arrival);
+        // $('#name-input').clear()
+        // $('#dest-input').clear()
+        // $('#freq-input').clear()
+        // $('#time-input').clear()
+    });
+
+    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+    dataRef.ref().on("child_added", function(childSnapshot) {
+            // Console log each user input
+            console.log(childSnapshot.val().name);
+            console.log(childSnapshot.val().destination);
+            console.log(childSnapshot.val().frequency);
+            console.log(childSnapshot.val().arrival);
+
+            // full list of items to the well
+            $("#display-row").append("<tr><td id='name-display'>" + childSnapshot.val().name + "</td>" + "<td id='dest-display'>" + childSnapshot.val().destination + "</td>" + "<td id='freq-display'>" + childSnapshot.val().frequency + "</td>" + "<td id='minutes-display'>" + childSnapshot.val().arrival + "</td></tr>");
+
+
+            // Handle the errors
+        },
+        function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+        });
+    dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+        // Change the HTML to reflect
+        $("#name-display").text(snapshot.val().name);
+        $("#dest-display").text(snapshot.val().destination);
+        $("#freq-display").text(snapshot.val().frequency);
+        $("#arrival-display").text(snapshot.val().arrival);
+    });
